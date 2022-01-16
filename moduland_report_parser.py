3662860c466ec762a9fce73114a4aea29e0eb1da
Rachel Duffin <rachel.g.duffin@gmail.com>
2022-01-16 14:19:46 +0000

    Add id to gene name conversion functionality

--- moduland_report_parser.py
+++ moduland_report_parser.py
@@ -1,11 +1,13 @@
 #!/bin/python3
 
 import sys
+from GenePanelAnalyzer import ncbi_id_to_genename, file_to_list
 
 def main():
     suid_file = sys.argv[1]
     moduland_report = sys.argv[2]
-    out = sys.argv[3]
+    gene_name_file = sys.argv[3]
+    out = sys.argv[4]
 
     # for line after
     suid_list = []
@@ -19,18 +21,28 @@ def main():
                     suid_list.append(id)
 
     suid_list = set(suid_list)
-    out_file = []
+    final_id_list = []
 
     with open(suid_file) as suid_file:
         for line in suid_file:
             elements = line.split(',')
             for ID in suid_list:
                 if ID == elements[0]:
-                    out_file.append(elements[1] + "\n")
+                    final_id_list.append(elements[1])
+    print(final_id_list)
+    predicted_gene_names = ncbi_id_to_genename(final_id_list, "predicted gene names")
+    print("PRed")
+    print(predicted_gene_names)
+    green_gene_namelist = file_to_list(gene_name_file)
+    print("green")
+    print(green_gene_namelist)
+    combined_genelists = green_gene_namelist + predicted_gene_names
+    print("combined")
+    print(combined_genelists)
 
-    with open(out, "w") as f:
-        f.writelines(out_file)
-
+    with open(out, "w") as file:
+        for gene in combined_genelists:
+            file.write(gene + "\n")
 
 if __name__ == "__main__":
     main()
